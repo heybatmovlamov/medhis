5ad3d3ef58266966ca0665416de54f7e8006c216
Yusif <yusif.asadli2003@gmail.com>
2025-07-08 16:10:10 +0400

    Şirkətə push üçün ilkin commit

--- src/main/java/com/qrcoderesultaccess/dao/repository/PatientRepository.java
+++ src/main/java/com/qrcoderesultaccess/dao/repository/PatientRepository.java
@@ -10,7 +10,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-
 public interface PatientRepository extends JpaRepository<PatientIdEntity, Integer> {
 
     @Query(value = "CALL check_new_lis_reports_patient()", nativeQuery = true)
